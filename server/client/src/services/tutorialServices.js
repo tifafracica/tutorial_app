9a5ba6c7aca81543bfc01e5c86b56f1ebdaf45b7
Stefanny Paola Fracica Barboza <stefanny.fracica@ibm.com>
2021-08-01 12:27:41 -0300

    try to fix problem in app

--- server/client/src/services/tutorialServices.js
+++ server/client/src/services/tutorialServices.js
@@ -5,7 +5,8 @@ const instance = axios.create({
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": 'https://tifa-tutorial-app.herokuapp.com'
-  });
+  }
+});
 
 // We call axios (in a const as instance) get, post, put, delete method corresponding to HTTP Requests: GET, POST, PUT, DELETE to make CRUD Operations.
 
